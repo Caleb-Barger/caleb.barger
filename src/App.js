fb82076fe9e35521dee5bbb88b20298de9f2c732
Caleb <caleb@pop-os.localdomain>
2021-01-12 08:16:29 -0800

    finnaly creating a repo for this project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+    Link,
+    Switch,
+    Route
+} from 'react-router-dom';
 import './App.css';
+
+import Post1 from './Post1';
+
+const Posts = () => {
+    return (
+        <>
+            <div className="posts">
+                <h2>Hello? yeah not much here yet. More coming soon!</h2>
+                <div className="postSummary">
+                    <Link className="postLink" to="/post1">Some awesome blog title</Link>
+                    <p>7 January, 2021</p>
+                    <p>some summary about a particular post idea or whatever.</p>
+                </div>
+                <div className="postSummary">
+                    <Link className="postLink" to="/post1">Another awesome blog title</Link>
+                    <p>8 January, 2021</p>
+                    <p>some cool description lol.</p>
+                </div>
+                <div className="postSummary">
+                    <Link className="postLink" to="/post1">Final awesome blog title</Link>
+                    <p>9 January, 2021</p>
+                    <p>idea or whatever.</p>
+                </div>
+            </div>
+        </>);
+}
+
+// Nice little header
+// subtitle                                             posts
+//                    nice centered line break
+
+//                      Centered content
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="header">
+        <div className="headerText">
+            <h1>{'{ caleb.blog }'}</h1> 
+            <p>Potatoes from Idaho</p>
+        </div>
+        <Link className="postsLink" to="/">posts</Link>
+        </div>
+        <hr/>
+        <Switch>
+            <Route path="/post1">
+                <Post1/>
+            </Route>
+            <Route path="/">
+                <Posts/>
+            </Route>
+        </Switch>
+        <div className="footer">
+            Boring footer stuff
+        </div>
+    </>
   );
 }
 
